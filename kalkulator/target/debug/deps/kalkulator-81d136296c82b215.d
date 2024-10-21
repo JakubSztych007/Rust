8e19ab63db82e38f78fcefeb05782646d8846cc6
JakubSztych007 <jakub.sztych@op.pl>
2024-10-22 00:16:36 +0200

    kalkulator

---
+++ kalkulator/target/debug/deps/kalkulator-81d136296c82b215.d
@@ -0,0 +1,5 @@
+C:\Users\jakub\Rust\kalkulator\target\debug\deps\libkalkulator-81d136296c82b215.rmeta: src/main.rs
+
+C:\Users\jakub\Rust\kalkulator\target\debug\deps\kalkulator-81d136296c82b215.d: src/main.rs
+
+src/main.rs:
---
+++ kalkulator/target/debug/deps/kalkulator-8dc968b44972a90f.d
@@ -0,0 +1,5 @@
+C:\Users\jakub\Rust\kalkulator\target\debug\deps\libkalkulator-8dc968b44972a90f.rmeta: src/main.rs
+
+C:\Users\jakub\Rust\kalkulator\target\debug\deps\kalkulator-8dc968b44972a90f.d: src/main.rs
+
+src/main.rs:
