781d0d7c8d92373f3c4dbda2563d2e8f570bdc60
JakubSztych007 <jakub.sztych@op.pl>
2024-10-24 10:55:18 +0200

    wlasny iterator

--- kalkulator/src/main.rs
+++ kalkulator/src/main.rs
@@ -20,53 +20,72 @@ fn main() {
 
     println!("Podaj współczynnik a");
     let mut inputa = String::new();
-    io::stdin().read_line(&mut inputa).expect("Nie udałos ię odczytać linii");
+    io::stdin()
+        .read_line(&mut inputa)
+        .expect("Nie udałos ię odczytać linii");
 
     println!("Podaj współczynnik b");
     let mut inputb = String::new();
-    io::stdin().read_line(&mut inputb).expect("Nie udałos ię odczytać linii");
+    io::stdin()
+        .read_line(&mut inputb)
+        .expect("Nie udałos ię odczytać linii");
     
-    let  inputa: i32 = inputa.trim().parse().expect("Proszę podać pierwszą liczbę: ");
-    let  inputb: i32 = inputb.trim().parse().expect("Proszę podać drugą liczbę: ");
+    let  inputa: i32 = inputa
+        .trim()
+        .parse()
+        .expect("Proszę podać pierwszą liczbę: ");
+    let  inputb: i32 = inputb
+        .trim()
+        .parse()
+        .expect("Proszę podać drugą liczbę: ");
 
     println!("Jaką operację chcesz wykonać : dodawanie (+) , odejmowanie (-), mnożenie (*), dzielenie (/)");
     let mut operacja = String::new();
-    io::stdin().read_line(&mut operacja).expect("Nie udało ci się odczytać linii");
+    io::stdin()
+        .read_line(&mut operacja)
+        .expect("Nie udało ci się odczytać linii");
 
 
-    const textdod:&str = "+";
-    const textode:&str = "-";
-    const textmn:&str = "*";
-    const textdziel:&str = "/";
+    const TEXTDOD:&str = "+";
+    const TEXTODE:&str = "-";
+    const TEXTMN:&str = "*";
+    const TEXTDZIEL:&str = "/";
 
-    match operacja.as_str().trim() {
-        textdod => {
+    match operacja
+        .as_str()
+        .trim() {
+        TEXTDOD => {
 
             let wynik = suma(inputa, inputb);
 
-            println!("Wynik dodawania współczynników: {} oraz {} to {}", inputa, inputb, wynik);
+            println!("Wynik dodawania współczynników: {} oraz {} to {}",
+            inputa, inputb, wynik);
         }
         
-        textode => {
+        TEXTODE => {
 
             let wynik = roznica(inputa, inputb);
 
-            println!("Wynik odejmowania współczynników: {} oraz {} to {}", inputa, inputb, wynik);
+            println!("Wynik odejmowania współczynników: {} oraz {} to {}",
+            inputa, inputb, wynik);
         }
-        textmn => {
+        TEXTMN => {
 
             let wynik = iloczyn(inputa, inputb);
 
-            println!("Wynik mnożenia współczynników: {} oraz {} to {}", inputa, inputb, wynik);
+            println!("Wynik mnożenia współczynników: {} oraz {} to {}",
+            inputa, inputb, wynik);
         }
-        textdziel => {
-            if inputb == 0{
-                println!("Wynik dzielenia współczynników: {} oraz {} to 0", inputa, inputb);
+        TEXTDZIEL => {
+            if inputb != 0 {
+                let wynik = iloraz(inputa, inputb);
+
+                println!("Wynik mnożenia współczynników: {} oraz {} to {}",
+                inputa, inputb, wynik);
             
             }else{
-            let wynik = iloraz(inputa, inputb);
 
-            println!("Wynik dzielenia współczynników: {} oraz {} to {}", inputa, inputb, wynik);
+                println!("Wynik dzielenia współczynników: {} oraz {} to 0", inputa, inputb);
             }
         }
         _ =>{
